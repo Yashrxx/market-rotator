0a20aa7947045ed1af487631b9615a3c085be776
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-10-19 22:32:09 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/market-rotator">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
